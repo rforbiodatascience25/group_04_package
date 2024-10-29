89e9cca9b624dfc909d4ecd9a84420418d86304b
TilNelissen <s233043@dtu.dk>
2024-10-29 16:18:02 +0100

    Fixing stuff

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Title: Tabels of codons
+#'
+#' Description: A table containing all codons and their corresponding amino acids one letters code
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codons"
