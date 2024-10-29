14845c2e3bce85b3e584f8a4ed66a1339c6267d3
TilNelissen <s233043@dtu.dk>
2024-10-29 16:25:24 +0100

    Added Roxygen Skeleton

--- R/translation_fun.R
+++ R/translation_fun.R
@@ -1,3 +1,12 @@
+#' Translation Function
+#'
+#' @param codons String vector with codons, separated by a comma (?)
+#'
+#' @return Returns a string with the amino acid sequence based on the vector given.
+#' @export
+#'
+#' @examples input_RNA <- c(AUG, UUG, GUG, GAU, GCC)
+#' translation_RNA_2_AA(input_RNA)
 translation_RNA_2_AA <- function(codons){
   AA_seq <- paste0(codons[codons], collapse = "")
   return(AA_seq)
