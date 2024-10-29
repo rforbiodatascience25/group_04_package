0db46f25df452a6e086637e81f8c34cacc327f8a
TilNelissen <s233043@dtu.dk>
2024-10-29 16:34:50 +0100

    Changed some characters and corrected the example

--- R/translation_fun.R
+++ R/translation_fun.R
@@ -1,13 +1,13 @@
 #' Translation Function
 #'
-#' @param codons String vector with codons, separated by a comma ?
+#' @param codons String vector with codons, separated by a comma
 #'
-#' @return Returns a string with the amino acid sequence based on the vector given.
+#' @return Returns a string with the amino acid sequence based on the vector given
 #' @export
 #'
-#' @examples input_RNA <- c(AUG, UUG, GUG, GAU, GCC)
+#' @examples input_RNA <- c("AUG", "UUG", "GUG", "GAU", "GCC")
 #' translation_RNA_2_AA(input_RNA)
-translation_RNA_2_AA <- function(codons){
-  AA_seq <- paste0(codons[codons], collapse = "")
+translation_RNA_2_AA <- function(input_codons){
+  AA_seq <- paste0(codons[input_codons], collapse = "")
   return(AA_seq)
 }
