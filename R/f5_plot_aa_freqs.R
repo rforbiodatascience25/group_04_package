f21ce2452ea34d194b9d3a7117ffcdeca00ae70f
USERNAME <USEREMAIL@EXAMPLE.ORG>
2024-10-30 22:49:11 +0100

    Package setup

--- R/f5_plot_aa_freqs.R
+++ R/f5_plot_aa_freqs.R
@@ -2,6 +2,9 @@
 
 #' Title: Plotting amino acid frequency
 #'
+#' @import ggplot2
+#' @import From stringr str_split boundary str_count
+#'
 #' @param aa_sequence An amino acid seqence of any length.
 #'
 #' @return Returns colored box plot with counts for each appearing amino acid.
@@ -11,19 +14,21 @@
 #'
 
 plot_aa_freqs <- function(aa_sequence){
+
+
   aa_names <- aa_sequence |>
     stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
-    as.character() |>
-    unique()
+    as.character() |> # Split amino acid string into list of individual characters
+    unique()  # Remove repeating characters
 
 
   counts <- sapply(aa_names, function(amino_acid) stringr::str_count(string = aa_sequence, pattern =  amino_acid)) |>
-    as.data.frame()
+    as.data.frame() # Make dataframe containing counts of unique amino acids
 
-  colnames(counts) <- c("Counts")
-  counts[["aa_sequence"]] <- rownames(counts)
+  colnames(counts) <- c("Counts") # Naming column in dataframe
+  counts[["aa_sequence"]] <- rownames(counts) # Naming rows after after corresponding unique amino acids
 
-  plot <- counts |>
+  plot <- counts |> # Plot the found amino acid frequencies into box plot
     ggplot2::ggplot(ggplot2::aes(x = aa_sequence, y = Counts, fill = aa_sequence)) +
     ggplot2::geom_col() +
     ggplot2::theme_bw() +
